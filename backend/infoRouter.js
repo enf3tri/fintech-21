c681d9517b31afcee8f90e4f8275fdf0d83bc941
JerryHu87 <56461496+JerryHu87@users.noreply.github.com>
2021-04-24 16:49:08 -0400

    Update infoRouter.js

--- backend/infoRouter.js
+++ backend/infoRouter.js
@@ -1,1 +1,12 @@
 const express = (require 'express');
+const path = require('path');
+const router = expredss.Router()
+const{
+  getAllTrainees,
+  getOneTrainee,
+  createTrainee,
+  updateTrainee,
+  deleteTRainee
+} = require('../controllers/traineeController');
+router.get()
+  
